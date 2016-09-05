e3110fe971aeb1bfb17d7d93c45ccad7442a8f94
Vivian P <vivianphou@me.com>
2016-09-05 16:30:44 -0700

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Vivian Phou on 9/3/16.
+//  Copyright © 2016 Vivo Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
+
